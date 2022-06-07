a60e201568dc69cfb6e86c03400fd9a59443dcc5
Steelver <1steelver@gmail.com>
2022-06-07 17:47:38 +0000

    restart

---
+++ src/js/component/cards.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+
+export default function Card({ name, url, typeCard, id }) {
+  return (
+    <div>
+      <div className="card" style={{ width: "18rem" }}>
+        <img className="card-img-top" src={url} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <Link to={typeCard === "characters" ? `/people/${id}` : "/planets"}>
+            <span className="btn btn-primary btn-lg" href="#" role="button">
+              Learn more
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,30 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useContext } from "react";
 import "../../styles/home.css";
+import Card from "../component/Card";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+  console.log(store.characters);
+  //actions.verPersona(id)
+  return (
+    <div className="mt-5 container">
+      <div className="overflow-x">
+        <h1 className="text-danger">Characters</h1>
+        <div className=" d-flex">
+          {store.characters.map((element, index) => {
+            return <Card key={element.uid} typeCard="characters" name={element.name} id={element.uid} url={store.imgCharacters[index].url} className="col-3" />
+          })}
+          
+        </div>
+      </div>
+      <h1 className="text-danger">Planets</h1>
+      <div className=" d-flex">
+          {store.planets.map((element, index) => {
+            return <Card key={element.uid} typeCard="planets" name={element.name} id={element.uid} url={store.imgPlanets[index].url} className="col-3" />
+          })}
+          
+        </div>
+    </div>
+  );
+};
