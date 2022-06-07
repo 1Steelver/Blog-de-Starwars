ad33066adab98c38e9e78861d839895552c4a78c
Steelver <1steelver@gmail.com>
2022-06-07 17:53:16 +0000

    rrr

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,6 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import CharacterDetails from "./views/characterDetails"
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -25,17 +26,11 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
-							<Demo />
+						<Route exact path="/people/:ids">
+							<CharacterDetails />
 						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
-						</Route>
-						<Route>
-							<h1>Not found!</h1>
-						</Route>
+						
 					</Switch>
-					<Footer />
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
